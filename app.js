bc77973b206bdca7f146b64d251bf124493e81f3
ZeYuanDuan <zyd.zeyuanduan@gmail.com>
2024-06-12 14:45:28 +0800

    docs: adding .gitignore

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const session = require("express-session");
 const flash = require("connect-flash");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const router = require("./routes");
 const passport = require("./config/passport");
