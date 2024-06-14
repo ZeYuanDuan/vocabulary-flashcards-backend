7d5abc0a6207fccd1a9d722826e99c69a8c9264a
ZeYuanDuan <zyd.zeyuanduan@gmail.com>
2024-06-14 11:37:54 +0800

    fix: no redirect within routers

--- middlewares/authHandler.js
+++ middlewares/authHandler.js
@@ -2,5 +2,11 @@ module.exports = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
-  return res.redirect("/login");
+  // const message = req.flash("error");
+  // res.json({
+  //   message: message.length > 0 ? message[0] : "請先登入",
+  // });
+  res.json({
+    message: "請先登入",
+  });
 };
--- routes/index.js
+++ routes/index.js
@@ -26,28 +26,39 @@ router.get("/", authHandler, async (req, res, next) => {
   }
 });
 
-router.get("/login", (req, res) => {
-  const message = req.flash("error");
-  res.json({
-    message: message.length > 0 ? message[0] : "請先登入",
-  });
-});
+// router.get("/login", (req, res) => {
+//   const message = req.flash("error");
+//   res.json({
+//     message: message.length > 0 ? message[0] : "請先登入",
+//   });
+// });
 
-router.post("/login", (req, res, next) => {
+router.post("/login", async (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
     if (err) {
       return next(err);
     }
     if (!user) {
-      req.flash("error", info.message);
-      return res.redirect("back");
+      // req.flash("error", info.message);
+      // return res.redirect("back");
+      return res.status(401).json({ message: info.message });
     }
-    return req.login(user, (err) => {
+    return req.login(user, async (err) => {
       if (err) {
         return next(err);
       }
-      req.flash("success", info.message);
-      return res.redirect("/");
+      // req.flash("success", info.message);
+      // return res.redirect("/");
+      try {
+        const { name, id } = user;
+        const message = info.message;
+        const vocStorage = await Vocabulary.count({
+          where: { UserId: id },
+        });
+        return res.json({ message, name, vocStorage });
+      } catch (err) {
+        next(err);
+      }
     });
   })(req, res, next);
 });
