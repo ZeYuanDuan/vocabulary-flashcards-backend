f50f4284013751dd8265a0547afacc3b170008c0
ZeYuanDuan <zyd.zeyuanduan@gmail.com>
2024-08-09 11:56:20 +0800

    test: no Google_Client_ID

--- routes/index.js
+++ routes/index.js
@@ -56,15 +56,16 @@ router.get("/daily", homeController.getDailyVocabularies);
 //   }
 // );
 
-router.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-    prompt: "select_account",
-  })
-);
+// ! 因為容器部屬沒找到 GOOGLE_CLIENT_ID，暫時關閉做測試
+// router.get(
+//   "/auth/google",
+//   passport.authenticate("google", {
+//     scope: ["profile", "email"],
+//     prompt: "select_account",
+//   })
+// );
 
-router.get("/auth/google/callback", authController.getGoogleAuthCallback);
+// router.get("/auth/google/callback", authController.getGoogleAuthCallback);
 
 // ===========================
 
