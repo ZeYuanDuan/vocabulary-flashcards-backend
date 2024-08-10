4700c36df58ede7953416124d047d2c8b9abdb93
ZeYuanDuan <zyd.zeyuanduan@gmail.com>
2024-08-10 23:43:22 +0800

    fix: type (change app into router)

--- routes/index.js
+++ routes/index.js
@@ -67,7 +67,7 @@ router.get(
 router.get("/auth/google/callback", authController.getGoogleAuthCallback);
 
 // ! Loader.io 驗證 (壓力測試用)
-app.get("/loaderio-559d14be60679a0e8cb038a519654045/", (req, res) => {
+router.get("/loaderio-559d14be60679a0e8cb038a519654045/", (req, res) => {
   res.send("loaderio-559d14be60679a0e8cb038a519654045");
 });
 
