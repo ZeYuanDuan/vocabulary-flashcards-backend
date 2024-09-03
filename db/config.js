e2fb63e559f6e18be916a32d53e354de31cc765d
ZeYuanDuan <zyd.zeyuanduan@gmail.com>
2024-09-04 02:04:08 +0800

    chore(db/config.js): NODE_ENV-related settings and database configurations

--- db/config.js
+++ db/config.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 
+// **提示**: 為了確保不同環境（例如開發環境、測試環境和生產環境）使用不同的資料庫，請為每個環境設定各自的環境變數。
+// 例如，您可以為開發環境設定 `DB_NAME`，為測試環境設定 `DB_TEST_NAME`，為生產環境設定 `DB_PROD_NAME`。
+// 這樣做有助於避免數據混淆，並且提高應用程序的穩定性和安全性。
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
@@ -11,7 +15,7 @@ module.exports = {
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_TEST_NAME,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
   },
